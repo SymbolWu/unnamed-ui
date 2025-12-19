87e7de6cb1454a4b35bc9e70ecb36d8205980994
Fan, Yi-Jie <fancn21th@aliyun.com>
2025-12-19 10:50:35 +0800

    📖 DOC:  更细节的文档

--- apps/www/app/(home)/page.tsx
+++ apps/www/app/(home)/page.tsx
@@ -1,3 +1,4 @@
+import { Separator } from "@/registry/wuhan/ui/separator";
 import Link from "next/link";
 
 export default function HomePage() {
@@ -5,7 +6,11 @@ export default function HomePage() {
     <div className="flex flex-col justify-center text-center flex-1">
       <h1 className="text-2xl font-bold mb-4">unnamed-ui / home</h1>
       <p>
-        You can open{" "}
+        打开{" "}
+        <Link href="/docs" className="font-medium underline">
+          /docs
+        </Link>{" "}
+        你可以看到 设计系统文档 | You can open{" "}
         <Link href="/docs" className="font-medium underline">
           /docs
         </Link>{" "}
@@ -13,7 +18,11 @@ export default function HomePage() {
       </p>
 
       <p>
-        You can open{" "}
+        打开{" "}
+        <Link href="/themes" className="font-medium underline">
+          /themes
+        </Link>{" "}
+        你可以看到 token / 主题 切换 | You can open{" "}
         <Link href="/themes" className="font-medium underline">
           /themes
         </Link>{" "}
@@ -21,18 +30,26 @@ export default function HomePage() {
       </p>
 
       <p>
-        You can open{" "}
+        打开{" "}
+        <Link href="/libs" className="font-medium underline">
+          /libs
+        </Link>{" "}
+        你可以看到 ai 聊天框架文档 ｜ You can open{" "}
         <Link href="/libs" className="font-medium underline">
           /libs
         </Link>{" "}
         and see the ai chat framework documentation.
       </p>
       <p>
-        You can open{" "}
+        打开{" "}
         <Link href="/compositions" className="font-medium underline">
           /compositions
         </Link>{" "}
-        and see the ai chat framework documentation.
+        你可以看到 不同 chat 的组合 ｜ You can open{" "}
+        <Link href="/compositions" className="font-medium underline">
+          /compositions
+        </Link>{" "}
+        and see the ai chat variants.
       </p>
     </div>
   );
